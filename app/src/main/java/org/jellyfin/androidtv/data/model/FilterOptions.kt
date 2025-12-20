2f2a26178dd1bd9cc044f3807ea4924501530f71
SAM42 <144408939+Sam42a@users.noreply.github.com>
2025-12-20 16:11:17 +0100

    Fix filter logic to apply only one filter at a time

--- app/src/main/java/org/jellyfin/androidtv/data/model/FilterOptions.kt
+++ app/src/main/java/org/jellyfin/androidtv/data/model/FilterOptions.kt
@@ -9,7 +9,10 @@ class FilterOptions {
 
 	val filters: Set<ItemFilter>
 		get() = buildSet {
-			if (isFavoriteOnly) add(ItemFilter.IS_FAVORITE)
-			if (isUnwatchedOnly) add(ItemFilter.IS_UNPLAYED)
+			if (isFavoriteOnly) {
+				add(ItemFilter.IS_FAVORITE)
+			} else if (isUnwatchedOnly) {
+				add(ItemFilter.IS_UNPLAYED)
+			}
 		}
 }
